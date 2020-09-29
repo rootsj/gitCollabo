0b7f5c339104c83fb5d49b2be25cfcf66c3a7ec9
won-js <wonjs0116@gmail.com>
2020-09-29 15:16:29 +0900

    updated namerge

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import subtract
 import multiply
 import add
 import divide
+import namerge
 
 print("사칙연산을 선택 하세요.")
 print("1.더하기")
 print("2.빼기")
 print("3.곱하기")
 print("4.나누기")
+print("5.나머지 계산")
 
 choice = input("선택 하세요(1/2/3/4):")
 
@@ -25,5 +27,7 @@ elif choice == '3':
 
 elif choice == '4':
    print(num1,"/",num2,"=", divide.divide(num1,num2))
+elif choice == '5':
+   print(num1,"%",num2,"=", namerge.namerge(num1,num2))
 else:
    print("잘못된 선택")
---
+++ namerge.py
@@ -0,0 +1,4 @@
+def namerge(x, y):
+    if y == 0:
+        return '0으로 나눌 수 없습니다.'
+    return x % y
